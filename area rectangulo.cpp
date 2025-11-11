68cabcabee706592184ea318ba354e6c02845423
a01798595-prog <a01798595@tec.mx>
2025-11-11 06:45:27 -0600

    Add files via upload

---
+++ area rectangulo.cpp
@@ -0,0 +1,20 @@
+// Created by Larissa Mendoza Rios A01798595
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int base, altura, area;
+
+    cout << "Ingrese la base del rectángulo: ";
+    cin >> base;
+
+    cout << "Ingrese la altura del rectángulo: ";
+    cin >> altura;
+
+    area = base * altura;
+
+    cout << "El área del rectángulo es: " << area << endl;
+
+    return 0;
+}
